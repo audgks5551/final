47b5d568ef784fa8be2b559baeb556a4556b2865
audgks5551 <audgks5551@daum.net>
2021-12-14 00:19:38 +0900

    django와 mariadb 연결, django가 mariadb를 기다리는 wait.sh 추가

---
+++ wait.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+apt-get update
+apt-get install netcat-openbsd
+while ! nc -z db 3306 ; do
+    echo "Waiting for the MariaDB Server"
+    sleep 3
+done
+
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
